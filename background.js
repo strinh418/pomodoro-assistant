ae2a0b1e00a748fc656c1b2610115284ff042b1d
Stephanie Trinh <stephanie_trinh@berkeley.edu>
2021-12-29 20:00:10 -0800

    Working focus and break intervals when popup is closed

--- background.js
+++ background.js
@@ -1,17 +1,123 @@
 let session = 'Focus';
 let focus_time = 25;
-let minute = 25;
-let second = 0;
+let short_break = 5;
+let long_break = 15;
 let timer_status = 'Not Started';
 let start_time = null;
 let end_time = null;
 let time_remaining = null; // used for pauses
+let intervals = 4;
+let completed = 0;
+
+let alarm = null;
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ session });
     chrome.storage.sync.set({ focus_time });
+    chrome.storage.sync.set({ short_break });
+    chrome.storage.sync.set({ long_break });
     chrome.storage.sync.set({ timer_status });
     chrome.storage.sync.set({ start_time });
     chrome.storage.sync.set({ end_time });
-    chrome.storage.sync.set({ time_remaining })
+    chrome.storage.sync.set({ time_remaining });
+    chrome.storage.sync.set({ intervals });
+    chrome.storage.sync.set({ completed });
 })
+
+// Alarm listener for timer
+chrome.storage.onChanged.addListener(function (changes, area) {
+    if (area === 'sync' && changes.end_time?.newValue) {
+        console.log("end time changed");
+        end_time = changes.end_time.newValue;
+        chrome.alarms.create('IntervalTimer', {
+            when: end_time
+        });
+        console.log("alarm created");
+    }
+  });
+chrome.alarms.onAlarm.addListener(timerListener);
+console.log("listener added");
+
+function timerListener(alarm) {
+    if (alarm.name === 'IntervalTimer') {
+        console.log("Timer finished!")
+        chrome.storage.sync.get([
+            'session',
+            'focus_time',
+            'short_break',
+            'completed',
+            'intervals'
+        ], function(variables) {
+            session = variables.session;
+            focus_time = variables.focus_time;
+            short_break = variables.short_break;
+            completed = variables.completed;
+            intervals = variables.intervals;
+            timer_status = 'Not Started';
+            const now = new Date();
+            // Finished a focus interval
+            if (session === 'Focus') {
+                completed += 1;
+                if (completed === intervals) {
+                    session = 'Long Break';
+                    chrome.notifications.create('long break'  + now.getTime(), {
+                        type: 'basic',
+                        iconUrl: 'images/tomatoes.png',
+                        title: 'Time for a longer break!',
+                        message: 'Great work on completing a cycle.',
+                        priority: 2
+                    });
+                    completed = 0;
+                } else {
+                    session = 'Short Break';
+                    chrome.notifications.create('short break'  + now.getTime(), {
+                        type: 'basic',
+                        iconUrl: 'images/tomatoes.png',
+                        title: 'Time for a break!',
+                        message: 'Great work on completing an interval.',
+                        priority: 2
+                    });
+                }
+                chrome.storage.sync.set({completed});
+            } else if (session === 'Short Break' || session === 'Long Break') {
+                session = 'Focus';
+                chrome.notifications.create('focus' + now.getTime(), {
+                    type: 'basic',
+                    iconUrl: 'images/tomatoes.png',
+                    title: 'Time to work!',
+                    message: 'Time to start another interval.',
+                    priority: 2
+                });
+            }
+            chrome.storage.sync.set({session, timer_status, completed});
+            chrome.alarms.clearAll();
+        });
+        /*const now = new Date();
+        if (session === 'Focus') {
+            chrome.notifications.create('focus' + now.getTime(), {
+                type: 'basic',
+                iconUrl: 'images/tomatoes.png',
+                title: 'Time to work!',
+                message: 'Time to start another interval.',
+                priority: 2
+            });
+        } else if (session === 'Short Break') {
+            chrome.notifications.create('short break'  + now.getTime(), {
+                type: 'basic',
+                iconUrl: 'images/tomatoes.png',
+                title: 'Time for a break!',
+                message: 'Great work on completing an interval.',
+                priority: 2
+            });
+        } else if (session === 'Long Break') {
+            chrome.notifications.create('long break'  + now.getTime(), {
+                type: 'basic',
+                iconUrl: 'images/tomatoes.png',
+                title: 'Time for a longer break!',
+                message: 'Great work on completing a cycle.',
+                priority: 2
+            });
+        }*/
+    }
+    
+}
--- popup.js
+++ popup.js
@@ -7,6 +7,8 @@ var timer_status;
 var start_time;
 var end_time;
 var focus_time;
+var short_break;
+var interval_time;
 var time_remaining;
 
 var second;
@@ -14,7 +16,11 @@ var minute;
 var ms = 0;
 var interval;
 
+// When popup first comes up from toolbar
 setupPopup();
+
+// Chrome Event Listeners
+chrome.storage.onChanged.addListener(storageListener);
 
 function setupPopup() {
     timer_btn.addEventListener('click', handleTimer);
@@ -22,23 +28,38 @@ function setupPopup() {
         'session',
         'timer_status',
         'focus_time',
+        'short_break',
+        'long_break',
         'end_time',
         'time_remaining'
     ], function(variables) {
         session = variables.session;
         timer_status = variables.timer_status;
         focus_time = variables.focus_time;
+        short_break = variables.short_break;
+        long_break = variables.long_break;
         end_time = variables.end_time;
         time_remaining = variables.time_remaining;
 
         console.log(timer_status);
-        status_bar.innerHTML = "Time to: " + session;
+        status_bar.innerHTML = "Time for: " + session;
         if (session === 'Focus') {
             timer.style.backgroundColor = '#ff6666';
+            interval_time = focus_time;
+        } else if (session === 'Short Break') {
+            timer.style.backgroundColor = '#39b535';
+            interval_time = short_break;
+        } else if (session === 'Long Break') {
+            timer.style.backgroundColor = '#63d0ff';
+            interval_time = long_break;
         }
         if (timer_status === 'Not Started') {
             timer_btn.innerHTML = 'Start';
-            timer.innerHTML = focus_time + ":00";
+            timer.innerHTML = interval_time + ":00";
+            if (interval) {
+                // clear any existing intervals
+                clearInterval(interval);
+            }
         } else if (timer_status === 'Paused') {
             timer_btn.innerHTML = 'Play';
             setRemainingTime(time_remaining);
@@ -47,8 +68,8 @@ function setupPopup() {
             timer_btn.innerHTML = 'Pause';
             setMinSec(end_time);
             timer.innerHTML = minute + ":" + checkSecond(second);
-            setTimeout(startTimer, ms);
-            interval = setInterval(startTimer, 1000);
+            setTimeout(countdown, ms);
+            interval = setInterval(countdown, 1000);
         } else {
             timer_btn.innerHTML = 'ERROR';
         }
@@ -75,12 +96,18 @@ function setRemainingTime(timeDiff) {
 }
 
 function handleTimer() {
+    // Timer currently not playing but the play button is pressed
     if (timer_status === 'Not Started') {
-        startFocusTimer();
+        console.log('handle timer: Not Started');
+        startTimer();
+    // Timer currently playing and the pause button is pressed
     } else if (timer_status === 'Playing') {
-        pauseFocusTimer();
+        console.log('handle timer: Playing');
+        pauseTimer();
+    // Timer currently paused and the play button is pressed
     } else if (timer_status === 'Paused') {
-        playFocusTimer();
+        console.log('handle timer: Paused');
+        playTimer();
     } else {
         timer_btn.innerHTML = 'ERROR';
         return;
@@ -88,26 +115,25 @@ function handleTimer() {
     chrome.storage.sync.set({timer_status});
 }
 
-function startFocusTimer() {
+function startTimer() {
     timer_status = 'Playing';
     timer_btn.innerHTML = 'Pause';
     start_time = new Date();
     end_time = new Date(start_time.getFullYear(), start_time.getMonth(), start_time.getDate(), start_time.getHours(), start_time.getMinutes(), start_time.getSeconds(), start_time.getMilliseconds());
-    end_time.setMinutes(end_time.getMinutes() + focus_time);
+    end_time.setMinutes(end_time.getMinutes() + interval_time);
 
     start_time = start_time.getTime();
     end_time = end_time.getTime();
     chrome.storage.sync.set({start_time});
     chrome.storage.sync.set({end_time});
     chrome.storage.sync.set({timer_status});
-
     second = 0;
-    minute = focus_time;
-    startTimer();
-    interval = setInterval(startTimer, 1000);
+    minute = interval_time;
+    countdown();
+    interval = setInterval(countdown, 1000);
 }
 
-function pauseFocusTimer() {
+function pauseTimer() {
     timer_status = 'Paused';
     timer_btn.innerHTML = 'Play';
     const now = new Date();
@@ -117,7 +143,16 @@ function pauseFocusTimer() {
     clearInterval(interval);
 }
 
-function playFocusTimer() { 
+function restartTimer() {
+    // timer_status = 'Not Started'; (Timer status now set in background.js)
+    timer.innerHTML = interval_time + ':00';
+    status_bar.innerHTML = "Time for: " + session;
+    //chrome.storage.sync.set({timer_status});
+    timer_btn.innerHTML = 'Start';
+    clearInterval(interval);
+}
+
+function playTimer() { 
     timer_status = 'Playing';
     timer_btn.innerHTML = 'Pause';
     const now = new Date();
@@ -126,12 +161,15 @@ function playFocusTimer() {
     chrome.storage.sync.set({end_time});
     chrome.storage.sync.set({timer_status});
 
-    setTimeout(startTimer, ms);
-    interval = setInterval(startTimer, 1000);
+    setTimeout(countdown, ms);
+    interval = setInterval(countdown, 1000);
 }
 
-function startTimer() {
+function countdown() {
     if (second == 0) {
+        if (minute == 0) {
+            return;
+        }
         second = 59;
     } else {
         second = second - 1;
@@ -143,3 +181,30 @@ function startTimer() {
 
     timer.innerHTML = minute + ":" + checkSecond(second);
 }
+
+function storageListener(changes, area) {
+    if (area === 'sync' && changes.session?.newValue && changes.timer_status?.newValue) {
+        session = changes.session.newValue;
+        console.log('Session successfully changed to ' + session);
+        // Switching from break to focus
+        if (session === 'Focus') {
+            timer.style.backgroundColor = '#ff6666';
+            interval_time = focus_time;
+        // Switching from focus to short break
+        } else if (session === 'Short Break') {
+            timer.style.backgroundColor = '#39b535';
+            interval_time = short_break;
+        } else if (session === 'Long Break') {
+            timer.style.backgroundColor = '#63d0ff';
+            interval_time = long_break;
+        }
+        timer_status = changes.timer_status.newValue;
+        restartTimer();
+    }
+}
+
+/*function timerStatusListener(changes, area) {
+    if (area === 'sync' && changes.timer_status?.newValue) {
+        timer_status = changes.timer_status.newValue;
+    }
+}*/
